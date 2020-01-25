bc68ad5ba68ae6718595b8d361e5397e0ed74de1
MrLincon <52606514+MrLincon@users.noreply.github.com>
2020-01-25 17:08:10 +0600

    n

--- NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.navigationdrawer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.navigationdrawer.NavDrawer.NavActivity1;
+import com.example.navigationdrawer.NavDrawer.NavActivity2;
+import com.example.navigationdrawer.NavDrawer.NavActivity3;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView toolbarTitle;
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbarTitle = findViewById(R.id.toolbar_title);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbarTitle.setText("Navigation Drawer");
+
+
+        //Navigation Drawer
+
+        navigationView = findViewById(R.id.navigation_view_left);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open, R.string.close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
+        NavigationItems();
+
+        //.................
+
     }
+
+    //Navigation Drawer Items
+
+    public void NavigationItems() {
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull final MenuItem menuItem) {
+
+                //Handler for closing navigation first
+                // and performing action onClick after a certain delay
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        switch (menuItem.getItemId()) {
+                            case R.id.activity1:
+                                Intent activity1 = new Intent(MainActivity.this, NavActivity1.class);
+                                startActivity(activity1);
+                                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);    //Animation for opening activity
+                                Toast.makeText(MainActivity.this, "Activity 1", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.activity2:
+                                Intent activity2 = new Intent(MainActivity.this, NavActivity2.class);
+                                startActivity(activity2);
+                                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);    //Animation for opening activity
+                                Toast.makeText(MainActivity.this, "Activity 2", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.activity3:
+                                Intent activity3 = new Intent(MainActivity.this, NavActivity3.class);
+                                startActivity(activity3);
+                                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);    //Animation for opening activity
+                                Toast.makeText(MainActivity.this, "Activity 3", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                }, 200);
+
+                //...............
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+    }
+
+    //................
 }
---
+++ OptionsMenu/app/src/main/java/com/example/optionsmenu/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.optionsmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
+    private TextView toolbarTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbarTitle = findViewById(R.id.toolbar_title);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbarTitle.setText("Options Menu");
+
+    }
+
+    //Options Menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //...................
+}
