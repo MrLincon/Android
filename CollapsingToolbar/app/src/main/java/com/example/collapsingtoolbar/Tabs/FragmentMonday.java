c7457906be439718b75c8f24bd912b89c0dbe51b
Ahamed Lincon <52606514+MrLincon@users.noreply.github.com>
2020-06-20 05:30:35 +0600

    Collapsing Toolbar

---
+++ CollapsingToolbar/app/src/main/java/com/example/collapsingtoolbar/Tabs/FragmentMonday.java
@@ -0,0 +1,26 @@
+package com.example.collapsingtoolbar.Tabs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.collapsingtoolbar.R;
+
+
+public class FragmentMonday extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_monday, container, false);
+
+        return view;
+    }
+}
